e2d9c14a4d368f45fa594032e54ec0b348c8f922
Martin Piliar <edino525@gmail.com>
2021-02-18 14:07:21 +0100

    initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float power;
+    public float rotationPower;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            rb.velocity = Vector2.zero;
+            rb.AddForce(Vector2.up * power,ForceMode2D.Impulse);
+        }
+    }
+}
