174eb64d2d345d5e452dc676a3e4b5354bf3347a
Peter Pagáč <peter.teo.pagac@gmail.com>
2021-02-21 21:41:52 +0100

    latest version

---
+++ Assets/Scripts/LevelMover.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelMover : MonoBehaviour
+{
+    public float moveSpeed = 5;
+    
+    void Update()
+    {
+        transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,19 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
     private Rigidbody2D rb;
     public float power;
     public float rotationPower;
-    // Start is called before the first frame update
+
+    public int enemyLayer = 10;
+    public float spawnColumnDelay = 3;
+    public GameObject columnPrefab;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        StartCoroutine(SpawnObstaclesCoroutine());
     }
 
-    // Update is called once per frame
+    IEnumerator SpawnObstaclesCoroutine()
+    {
+        while(true)
+        {
+            GameObject col = Instantiate(columnPrefab);
+            col.transform.position = new Vector3( 8, Random.Range(-2, 2), 0);
+            col.transform.rotation = Quaternion.identity;
+
+            yield return new WaitForSeconds(spawnColumnDelay);
+        }
+    }
+
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
@@ -22,4 +40,12 @@ public class Player : MonoBehaviour
             rb.AddForce(Vector2.up * power,ForceMode2D.Impulse);
         }
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == enemyLayer)
+            SceneManager.LoadScene( SceneManager.GetActiveScene().name );
+    }
+
 }
+
